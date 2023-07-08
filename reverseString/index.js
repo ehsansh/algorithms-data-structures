172f88e957105f3866d95a76b4474ce9b0476d2a
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-08 19:48:53 +0330

    reverse a string using built in reverse fn

--- reverseString/index.js
+++ reverseString/index.js
@@ -4,7 +4,9 @@
 //   reverse('abcd') === 'dcba'
 //   reverse('Hello!') === '!olleH'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -33,10 +35,10 @@ mocha.setup('bdd');
 const { assert } = chai;
 
 describe('String Reversal', () => {
-	it('reverse() correctly reverses string', () => {
-		assert.equal(reverse('ffaa'), 'aaff');
-		assert.equal(reverse('  aaff'), 'ffaa  ');
-	});
+    it('reverse() correctly reverses string', () => {
+        assert.equal(reverse('ffaa'), 'aaff');
+        assert.equal(reverse('  aaff'), 'ffaa  ');
+    });
 });
 
 mocha.run();
