598646309fd9e298492029f018f18d5003f04972
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-09 14:34:18 +0330

    solve anagrams algorithm problem

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,7 +8,40 @@
 //   anagrams('Heart!', 'EARTH') --> True
 //   anagrams('lol', 'lolc') --> False
 
-function anagrams(stringA, stringB) {}
+function createCharCountObj(str) {
+    let charCountObj = {};
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        charCountObj[char] = charCountObj[char] + 1 || 1;
+    }
+    return charCountObj;
+}
+
+function sanitizeString(str) {
+    return str
+        .replace(/[^a-zA-Z]+/g, '')
+        .toLowerCase()
+        .trim();
+}
+
+function anagrams(stringA, stringB) {
+    stringA = sanitizeString(stringA);
+    stringB = sanitizeString(stringB);
+
+    if (stringA.length !== stringB.length) return false;
+
+    let aCharObj = createCharCountObj(stringA);
+    let bCharObj = createCharCountObj(stringB);
+
+    for (let key in aCharObj) {
+        if (aCharObj[key] !== bCharObj[key]) {
+            return false;
+            break;
+        }
+    }
+
+    return true;
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -33,29 +66,29 @@ function anagrams(stringA, stringB) {}
 //                          ______ ______ ______ ______ ______
 //                         |______|______|______|______|______|
 
-mocha.setup("bdd");
+mocha.setup('bdd');
 const { assert } = chai;
 
-describe("Anagrams", () => {
-  it("works if case sensitivity and non word characters NOT taken into account", () => {
-    assert.equal(anagrams("earth", "heart"), true);
+describe('Anagrams', () => {
+    it('works if case sensitivity and non word characters NOT taken into account', () => {
+        assert.equal(anagrams('earth', 'heart'), true);
 
-    assert.equal(anagrams("love", "meow"), false);
-    assert.equal(anagrams("lol", "lolc"), false);
-  });
-  it("is case insensitive. 'HEART' and 'earth' should return true", () => {
-    assert.equal(anagrams("HEART", "earth"), true);
-    assert.equal(anagrams("heart", "EARTH"), true);
+        assert.equal(anagrams('love', 'meow'), false);
+        assert.equal(anagrams('lol', 'lolc'), false);
+    });
+    it("is case insensitive. 'HEART' and 'earth' should return true", () => {
+        assert.equal(anagrams('HEART', 'earth'), true);
+        assert.equal(anagrams('heart', 'EARTH'), true);
 
-    assert.equal(anagrams("love", "meow"), false);
-    assert.equal(anagrams("lol", "lolc"), false);
-  });
-  it("only matches word characters. 'heart!'' and '' earth' should return true", () => {
-    assert.equal(anagrams("heart!", " earth"), true);
+        assert.equal(anagrams('love', 'meow'), false);
+        assert.equal(anagrams('lol', 'lolc'), false);
+    });
+    it("only matches word characters. 'heart!'' and '' earth' should return true", () => {
+        assert.equal(anagrams('heart!', ' earth'), true);
 
-    assert.equal(anagrams("love", "meow"), false);
-    assert.equal(anagrams("lol", "lolc"), false);
-  });
+        assert.equal(anagrams('love', 'meow'), false);
+        assert.equal(anagrams('lol', 'lolc'), false);
+    });
 });
 
 mocha.run();
